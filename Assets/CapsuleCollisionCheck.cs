189ce1fe6e98933c1bd47fe1914dc4afd0221d7d
furkaninalcik <m.furkaninalcik@gmail.com>
2018-10-18 17:20:35 +0300

    added path and collision check

---
+++ Assets/CapsuleCollisionCheck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CapsuleCollisionCheck : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("capsule");
+
+        //Debug.Log(this.transform.position);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        //GameObject[] text =  GameObject.FindGameObjectsWithTag("GameOverText");
+        //text[0].SetActive(true);
+        Debug.Log("COLLISION Capsule");
+
+    }
+}
---
+++ Assets/PathManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("path");
+
+        //Debug.Log(this.transform.position);
+
+
+    }
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        //GameObject[] text =  GameObject.FindGameObjectsWithTag("GameOverText");
+        //text[0].SetActive(true);
+        Debug.Log("COLLISION Path");
+
+    }
+}
